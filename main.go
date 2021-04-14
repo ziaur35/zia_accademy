9713d641179e4d14965d3329f77405f147cbc196
“ziaur35” <ziaur6886@gmail.com>
2021-04-14 16:39:57 +0100

    go

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
-
-
 import"fmt"
-
-
 
 func main(){
 
-fmt.Println("Hello Bangladesh")
-//fmt.Println("just for test")
+var name string = "ziaur"
+var age int = 20
+var result float32 = 20.30
+
+fmt.Println(name,age,result)
+
 }
