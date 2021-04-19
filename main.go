b2e52c50ce348134151663d77094a9302ea48876
“ziaur35” <ziaur6886@gmail.com>
2021-04-19 10:48:30 +0100

    arry

--- main.go
+++ main.go
@@ -2,12 +2,34 @@ package main
 
 import"fmt"
 
+
 func main(){
 
-var name string = "ziaur"
-var age int = 20
-var result float32 = 20.30
+//var students [3]string
 
-fmt.Println(name,age,result)
+//students[0]="mostain sir"
+//students[1]="reza sir"
+//students[2]="ziaur"
+
+
+//fmt.Println(students)
+//fmt.Println(len(students))
+//fmt.Println(students[1])
+students := [...]string{"mostain sir", "reza sir", "ziaur"}
+fmt.Println(students, len(students))
+
+
+//var fruits []string
+
+//fruits = append(fruits, "apple", "banana", "mango")
+
+//fmt.Println(fruits, len(fruits))
+
+//fmt.Printf("%T\n", fruits)
+
+//fmt.Printf("%T", students)
+
+//a := reflect.Typeof(fruits).Kind().string()
+//fmt.Println(a)
 
 }
