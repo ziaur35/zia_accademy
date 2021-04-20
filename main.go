b5fe548add8699205a4986f6a6cf468a0d10eac6
“ziaur35” <ziaur6886@gmail.com>
2021-04-20 16:51:21 +0100

    map

--- main.go
+++ main.go
@@ -1,38 +1,19 @@
 package main
 
-import(
-"fmt" 
-"reflect"
-)
+import"fmt" 
+
 func main(){
 
-//var students [3]string
-//students[0]="mostain sir"
-//students[1]="reza sir"
-//students[2]="ziaur"
+//var x map[string]string
 
-//x := students[0:2]
-//fmt.Println(x)
+x := make(map[string]string)
 
-//x := make([]string, 0)
-//fmt.Println(x)
+x["name"] = "ziaur"
 
-//fmt.Println(students)
-//fmt.Println(len(students))
-//fmt.Println(students[1])
-//students := [...]string{"mostain sir", "reza sir", "ziaur"}
-//fmt.Println(students, len(students))
+x["height"] = "5.7"
 
-var bread []string
-bread = append(bread, "bread1", "bread2", "bread3")
-//fmt.Println(bread)
-//fmt.Println(bread, len(bread))
-//fmt.Printf("%T\n", bread)
-//fmt.Printf("%T", students)
+x["address"] = "dhaka"
 
-a := reflect.TypeOf(bread).Kind().String()
-fmt.Println(a)
-
-
+fmt.Println(x)
 
 }
