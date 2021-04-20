bbcfefe6dd64ae32d219f1ccf9e740be0d2f8b46
“ziaur35” <ziaur6886@gmail.com>
2021-04-20 11:34:24 +0100

    slice

--- main.go
+++ main.go
@@ -1,35 +1,38 @@
 package main
 
-import"fmt"
-
-
+import(
+"fmt" 
+"reflect"
+)
 func main(){
 
 //var students [3]string
-
 //students[0]="mostain sir"
 //students[1]="reza sir"
 //students[2]="ziaur"
 
+//x := students[0:2]
+//fmt.Println(x)
+
+//x := make([]string, 0)
+//fmt.Println(x)
 
 //fmt.Println(students)
 //fmt.Println(len(students))
 //fmt.Println(students[1])
-students := [...]string{"mostain sir", "reza sir", "ziaur"}
-fmt.Println(students, len(students))
+//students := [...]string{"mostain sir", "reza sir", "ziaur"}
+//fmt.Println(students, len(students))
 
-
-//var fruits []string
-
-//fruits = append(fruits, "apple", "banana", "mango")
-
-//fmt.Println(fruits, len(fruits))
-
-//fmt.Printf("%T\n", fruits)
-
+var bread []string
+bread = append(bread, "bread1", "bread2", "bread3")
+//fmt.Println(bread)
+//fmt.Println(bread, len(bread))
+//fmt.Printf("%T\n", bread)
 //fmt.Printf("%T", students)
 
-//a := reflect.Typeof(fruits).Kind().string()
-//fmt.Println(a)
+a := reflect.TypeOf(bread).Kind().String()
+fmt.Println(a)
+
+
 
 }
