ada66fdb64ebbd52c6f6cd27332c9316bb0ecd63
“ziaur35” <ziaur6886@gmail.com>
2021-04-23 06:38:05 +0100

    structs2

--- main.go
+++ main.go
@@ -2,23 +2,22 @@ package main
 
 import"fmt"
 
-type book struct{
+func main(){
+
+
+mormon := struct{
  title string
  author string
  isbn string
  price float32
  pages int
+}{
+title= "book of mormon",
+author= "sacred text",
+isbn= "9781537696775",
+price=35.05,
+pages=238,
 }
-func main(){
-var mormon book
-mormon.title= "book of mormon"
-mormon.author= "sacred text"
-mormon.isbn= "9781537696775"
-mormon.price=35.05
-mormon.pages=238
-
-
-
 fmt.Println(mormon)
 
 }
