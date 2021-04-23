b9f2df9447296c7d0213577d93729848aae37ce0
“ziaur35” <ziaur6886@gmail.com>
2021-04-23 06:24:58 +0100

    structs

--- main.go
+++ main.go
@@ -2,34 +2,23 @@ package main
 
 import"fmt"
 
-
+type book struct{
+ title string
+ author string
+ isbn string
+ price float32
+ pages int
+}
 func main(){
-
-//var students [3]string
-
-//students[0]="mostain sir"
-//students[1]="reza sir"
-//students[2]="ziaur"
-
-
-//fmt.Println(students)
-//fmt.Println(len(students))
-//fmt.Println(students[1])
-students := [...]string{"mostain sir", "reza sir", "ziaur"}
-fmt.Println(students, len(students))
+var mormon book
+mormon.title= "book of mormon"
+mormon.author= "sacred text"
+mormon.isbn= "9781537696775"
+mormon.price=35.05
+mormon.pages=238
 
 
-//var fruits []string
 
-//fruits = append(fruits, "apple", "banana", "mango")
-
-//fmt.Println(fruits, len(fruits))
-
-//fmt.Printf("%T\n", fruits)
-
-//fmt.Printf("%T", students)
-
-//a := reflect.Typeof(fruits).Kind().string()
-//fmt.Println(a)
+fmt.Println(mormon)
 
 }
